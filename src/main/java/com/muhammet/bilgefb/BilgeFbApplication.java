963e402ddab2808c71a6e62c7069545c48fd8259
Muhammet Ali KAYA <info@beemeapp.com>
2023-07-06 22:03:08 +0300

    gün sonu

---
+++ src/main/java/com/muhammet/bilgefb/BilgeFbApplication.java
@@ -0,0 +1,13 @@
+package com.muhammet.bilgefb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilgeFbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilgeFbApplication.class, args);
+	}
+
+}
